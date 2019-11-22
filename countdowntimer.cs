0246e67468cb9e66c71fb0f74d43f9e9cd2e01ba
Calen McCann <43736265+CalenMac@users.noreply.github.com>
2019-11-22 16:49:17 -0500

    Update countdowntimer.cs

--- countdowntimer.cs
+++ countdowntimer.cs
@@ -2,18 +2,32 @@ using System;
 
 namespace countdowntimer
 {
-    class countdown
+    class timer 
     {
         static void Main()
         {
-            for (int a = 10; a > 0; a--)
+            for (int a = 3; a >= 0; a--)
             {
-                Console.Write(" Your Computer will blow up in: {0}", a);
+                Console.Write("Your PC is getting hacked in: {0}", a);
                 System.Threading.Thread.Sleep(1000);
                 Console.Clear();
             }
-      
-        Console.Write ("BOOM!");
+            
+            for (int a = 0; a >= 0; a--)
+            {
+                Console.Write("Hacking status at 10 percent");
+                 System.Threading.Thread.Sleep(1000);
+                 Console.Clear();
+                Console.Write("Hacking status at 35 percent");
+                 System.Threading.Thread.Sleep(1000);
+                 Console.Clear();
+                Console.Write("Hacking status at 95 percent");
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+                
+            }
+            
+                Console.Write("All you saved passwords on this machine have been uploaded to my private site!");
+        }
     }
-  }
 }
